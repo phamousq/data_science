a611a2d065ba7bd46bcec9bb3f8584e91fe6aba5
q <phamousq@gmail.com>
2024-11-24 12:19:36 -0600

    clean up python and dependencies

---
+++ svelte/01.hello_counter/.svelte-kit/generated/client/app.js
@@ -0,0 +1,33 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [2],
+		"/about": [3],
+		"/counter": [4],
+		"/farenheight2celcius": [5],
+		"/flight-booker": [6],
+		"/projects": [7],
+		"/timer": [8]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.js';
